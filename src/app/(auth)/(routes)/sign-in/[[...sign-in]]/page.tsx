1b5caebdd1b2d9cab1fbec2b6011ffe044f0f14b
ZawNaingOo0 <zawnaingoo.mhs@gmail.com>
2023-11-09 22:57:05 +0630

    starterFile

---
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignIn } from "@clerk/nextjs";
+ 
+export default function Page() {
+  return (
+    <h1>Sign IN</h1>
+  );
+}
---
+++ src/app/(dashboard)/(group)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <>
+    <h1 className='font-bold text-red-500'>Protected page</h1>
+    </>
+    
+  )
+}
